6773f41047ecec51099bfa3ad5b02c2b0e2acffc
alexkors <korsun55555@mail.ru>
2022-04-24 14:57:19 +0700

    Первая программка

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// вывод квадрата заданного числа
+int x;
+Console.WriteLine("Введите число");
+x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Квадрат числа = ");
+Console.Write(x * x);
+
+
